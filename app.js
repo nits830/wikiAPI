7a242efc55b4b52b68a8112d02156676a8e94858
nits830 <nitishgupta830@gmail.com>
2023-05-28 11:50:45 +0530

    Basic functionality added

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/mywikiDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+const articleSchema = new mongoose.Schema({
+    title:String,
+    content:String
+})
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+
+
+app.get("/articles", async (req,res)=> {
+
+    const articles = await Article.find({});
+    res.send({status:"ok", data:articles});
+    
+})
+
+app.post('/articles', (req, res)=>{
+  
+  const newArticle = new Article({
+    title:req.body.title,
+    content:req.body.content
+  });
+  newArticle.save();
+  res.redirect("/articles");
+  
+});
+
+app.delete("/articles", async (req, res)=>{
+  await Article.deleteMany({});
+  res.redirect("/articles");
+});
+
+app.listen(3000, console.log("Server started"));
